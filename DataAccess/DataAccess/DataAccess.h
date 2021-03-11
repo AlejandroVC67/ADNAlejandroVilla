a6c424360d3b5aa3ddf8996ca881daf023ee6616
Alejandro Villa <vcalejandro97@gmail.com>
2021-03-11 11:22:46 -0500

    Adding third party libraries

---
+++ DataAccess/DataAccess/DataAccess.h
@@ -0,0 +1,18 @@
+//
+//  DataAccess.h
+//  DataAccess
+//
+//  Created by Alejandro Villa Cardenas - Ceiba Software on 11/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataAccess.
+FOUNDATION_EXPORT double DataAccessVersionNumber;
+
+//! Project version string for DataAccess.
+FOUNDATION_EXPORT const unsigned char DataAccessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataAccess/PublicHeader.h>
+
+
