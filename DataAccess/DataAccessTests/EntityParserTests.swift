2ead8ddbe56e3f66d46917dfcdceca1b3179594b
Alejandro Villa <vcalejandro97@gmail.com>
2021-03-12 13:06:44 -0500

    Adding EntityParser tests

---
+++ DataAccess/DataAccessTests/EntityParserTests.swift
@@ -0,0 +1,28 @@
+//
+//  EntityParserTests.swift
+//  DataAccessTests
+//
+//  Created by Alejandro Villa Cardenas - Ceiba Software on 12/03/21.
+//
+
+import XCTest
+import Domain
+import RealmSwift
+@testable import DataAccess
+
+class EntityParserTests: XCTestCase {
+    
+    func testTransformIntoEntity_givenVehicle_shouldReturnVehicleEntity() {
+        // Given
+        let vehicle = Vehicle(plates: "test", type: .car, cylinder: 100, startDate: Date())
+        
+        // When
+        let entity = EntityParser.transformIntoEntity(vehicle: vehicle)
+        
+        // Then
+        XCTAssertEqual(entity.plates, vehicle.plates, "The value should be \(vehicle.plates)")
+        XCTAssertEqual(entity.cylinder, vehicle.cylinder, "The value should be \(vehicle.cylinder)")
+        XCTAssertEqual(entity.type, vehicle.type.rawValue, "The value should be \(vehicle.type.rawValue)")
+        XCTAssertEqual(entity.startDate, vehicle.startDate, "The value should be \(vehicle.startDate)")
+    }
+}
